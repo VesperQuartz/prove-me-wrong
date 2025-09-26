299986c9e0f5e2eb67795272a7a39c6447695fb2
VesperQuartz <lilbrown3000@gmail.com>
2025-09-26 17:21:36 +0100

    init (6)

--- src/routes/(dashboard)/route.tsx
+++ src/routes/(dashboard)/route.tsx
@@ -1,43 +1,20 @@
 import { convexQuery } from "@convex-dev/react-query";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
-import { VapiWidget } from "@vapi-ai/client-sdk-react";
-import to from "await-to-ts";
 import { api } from "convex/_generated/api";
 import { Authenticated, useConvexAuth } from "convex/react";
-import ky from "ky";
-import React from "react";
-import { toast } from "sonner";
 import { NavBar } from "@/components/navbar";
-import { env } from "@/config/env";
-import { assistantOptions } from "@/data/assistant";
-import { checkAuth } from "@/middleware";
+import { Vapi } from "@/components/vapi";
 
 export const Route = createFileRoute("/(dashboard)")({
-	ssr: false,
+	ssr: "data-only",
 	component: RouteComponent,
-	server: {
-		middleware: [checkAuth],
-	},
 });
-
-type Msg = {
-	role: string;
-	content: string;
-};
 
 function RouteComponent() {
 	const { isLoading: isPending, isAuthenticated } = useConvexAuth();
-	const { data: user, isLoading } = useSuspenseQuery(
-		convexQuery(api.auth.currentUser, {}),
-	);
+	const { isLoading } = useSuspenseQuery(convexQuery(api.auth.currentUser, {}));
 	const router = useNavigate();
-	const userId = user?._id;
-	const firstName = user?.name?.split(" ")[0];
-	const [transcript, setTranscript] = React.useState<Array<Msg> | undefined>(
-		undefined,
-	);
-	console.log("hi");
 
 	if (isPending) {
 		return null;
@@ -63,59 +40,7 @@ function RouteComponent() {
 				<main className="mx-auto w-full max-w-7xl flex-1 p-4">
 					<Outlet />
 				</main>
-				{isLoading ? (
-					<div>Loading</div>
-				) : (
-					<VapiWidget
-						publicKey="7f4928e6-1600-4543-82a7-17fee85dc6f5"
-						mode="voice"
-						size="tiny"
-						assistantId="76a76ea4-4ed8-4520-b341-6c1ee4b575cd"
-						onMessage={(message) => {
-							console.log(message, ":::message-data:::");
-							if (message.type === "conversation-update") {
-								setTranscript(message.conversation);
-							}
-						}}
-						onVoiceEnd={async () => {
-							console.log("Call is ended!!!", transcript);
-							if (transcript) {
-								const data = transcript.filter(
-									(trans) => trans.role !== "system",
-								);
-								const [error, response] = await to(
-									ky.post(`${env.webhookUrl}`, {
-										json: { transcript: data, userId },
-									}),
-								);
-								if (error) {
-									console.error("There was an error", error);
-									toast.error(error.message);
-								}
-								console.log(await response.json());
-								toast.success("Argument has ended");
-							}
-						}}
-						assistantOverrides={{
-							variableValues: {
-								firstName,
-								userId,
-							},
-						}}
-						voiceShowTranscript={true}
-						onError={(e) => {
-							console.log("error", JSON.stringify(e, null, 2));
-							// @ts-expect-error e
-							const error = e?.error;
-							if (error) {
-								toast.error(error.errorMsg);
-							} else {
-								toast.error("Something went wrong. Please try again.");
-							}
-						}}
-						assistant={assistantOptions({ firstName: firstName ?? "" })}
-					/>
-				)}
+				{isLoading ? <div>Loading</div> : <Vapi />}
 			</div>
 		</Authenticated>
 	);
